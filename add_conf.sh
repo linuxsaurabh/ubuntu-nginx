40888025946ee2dd9a6565078472522a582bb91c
root <root@ip-10-0-0-45>
2020-06-08 10:26:50 +0000

    added new files

--- add_conf.sh
+++ add_conf.sh
@@ -5,6 +5,7 @@
 if [ $S3_Enabled == "True" ]
 then
 	aws s3 cp s3://$bucket/$path /data/www/index.html
+        aws s3 cp s3://$bucket/$config /etc/nginx/conf.d/default.conf
 else
 	echo "s3 is not enabled"
 fi
